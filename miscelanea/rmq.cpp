29ee98b7a3fede3791e58453a126ac89a4021af2
lucastdcj <lucastdcj@gmail.com>
2017-06-25 18:30:08 -0300

    Update rmq.cpp

--- miscelanea/rmq.cpp
+++ miscelanea/rmq.cpp
@@ -10,28 +10,28 @@ v[a..b]. Em caso de empate, é devolvido o maior índice.
 Caso queira o menor, descomente o = em pairmin()
 %*/
 using namespace std;
-#define N 100100
+#define MAXN 100100
 #define LOG 16     // piso de log2(N)
 
-int *va, Log2[N], p[LOG+1][N];
+int *va, Log2[MAXN], p[LOG+1][MAXN];
 
 int pairmin(int i1, int i2) {
-   return va[i1]</*=*/va[i2] ? i1:i2;
+   return va[i1] </*=*/va[i2] ? i1 : i2;
 }
 
 void init(int *a, int n) {
   va = a;
-  for (int i=1,k=0; i<=n; i++) {
+  for (int i = 1, k = 0; i <= n; i++) {
     Log2[i] = k;
-    if (1 << (k+1) == i) k++;
+    if (1 << (k + 1) == i) k++;
   }
 
   int ln = Log2[n];
-  for (int i=0; i<n; i++) p[0][i]=i;
-  for (int i=1; i<=ln; i++) 
-    for (int j=0; j + (1 << i) - 1 < n; j++) {
+  for (int i = 0; i < n; i++) p[0][i] = i;
+  for (int i = 1; i <= ln; i++) 
+    for (int j = 0; j + (1 << i) - 1 < n; j++) {
       int i1 = p[i-1][j];
-      int i2 = p[i-1][j+ (1 << i-1)];
+      int i2 = p[i-1][j+ (1 << i - 1)];
       p[i][j] = pairmin(i1, i2);
     }
 }
@@ -51,14 +51,13 @@ int main(){
    int vet[1000];
    
    scanf(" %d",&n);
-   for (i=0;i<n;i++) {
+   for (i = 0; i < n; i++) {
       scanf(" %d",&x);
-      vet[i]=x;
+      vet[i] = x;
    }
    
    init(vet, n);
-   printf("%d\n",query(0,n-1)); 
+   printf("%d\n", query(0, n - 1)); 
    /*imprime o maior indice de um menor elemento*/
-   
    return 0;
 }
