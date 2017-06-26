a33bbf7a9bbf132bcbb632c5ea6e891c3bf6529a
lucastdcj <lucastdcj@gmail.com>
2017-06-25 23:47:52 -0300

    Update biconnected.cpp

--- grafos/biconnected.cpp
+++ grafos/biconnected.cpp
@@ -20,14 +20,13 @@ vector<int> pilha;
 
 // OUTPUT
 int ncomp;
-vector<int> comps[MAXN]
+vector<int> comps[MAXN];
 
 void dfs(int u, int p, int& t) {
   vis[u] = true;
   num[u] = low[u] = t++;
   pilha.push_back(u);
-  for (size_t i = 0; i < adj[u].size(); i++) {
-    int v = adj[u][i];
+  for (int v : adj[u]) {
     if (v == p)  continue;
     if (vis[v]) {
       low[u] = min(low[u], num[v]);
@@ -39,7 +38,7 @@ void dfs(int u, int p, int& t) {
       if (low[v] >= num[u] && (num[u] > 0 || num[v] > 1)) IS_ART(u);
       if (low[v] >= num[u]) {
         comps[ncomp].push_back(u);
-        while (pilha.back() != v) {
+        while (comps[ncomp].back() != v) {
           comps[ncomp].push_back(pilha.back());
           pilha.pop_back();
         }
@@ -50,8 +49,7 @@ void dfs(int u, int p, int& t) {
 }
 
 void bcc() {
-  int t;
   ncomp = 0;
   for (int i = 0; i < n; i++) vis[i] = false;
-  for (int i = 0; i < n; i++) if (!vis[i]) dfs(i, -1, t = 0);
+  for (int i = 0, t; i < n; i++) if (!vis[i]) dfs(i, -1, t = 0);
 }
