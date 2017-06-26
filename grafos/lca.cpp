019af0a961a987b96e11bdffeae44bb797af48ef
lucastdcj <lucastdcj@gmail.com>
2017-06-26 15:40:00 -0300

    Update lca.cpp

--- grafos/lca.cpp
+++ grafos/lca.cpp
@@ -1,3 +1,11 @@
+void init() {
+  for (int j = 1; (1 << j) < n; j++)
+    for (int i = 0; i < n; i++)
+      if (p[j - 1][i] != -1) {
+        p[j][i] = p[j - 1][p[j - 1][i]]; 
+      }
+}
+
 int lca(int u, int v) {
   if (depth[u] < depth[v]) swap(u, v);
   for (int i = LOG; i >= 0; i--) {
