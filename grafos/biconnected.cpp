86e8c22c433433b09020a27e410f1807948064ea
lucastdcj <lucastdcj@gmail.com>
2017-06-25 22:54:50 -0300

    Update biconnected.cpp

--- grafos/biconnected.cpp
+++ grafos/biconnected.cpp
@@ -1,116 +1,57 @@
-/*%
-Pontes, Pontos de Articulação e Componentes Biconexas
-Autor: Igor Assis / Douglas Santos
-Complexidade: O(n+m)
-Testes: uva.796 spojbr.TUBOS
-Descricao: Encontra as pontes, os pontos de articulacao e as
-componentes biconexas (comecando em 1) em um grafo
-não direcionado. Não permite arestas múltiplas.
-%*/
-
-#include <cstring>
-#include <stack>
-#include <algorithm>
+#include <cstdio>
 #include <vector>
+#include <algorithm>
+#include <cstring>
+
 using namespace std;
 
-#define MAXN 1024
-#define MAXM 1024*1024
+typedef pair<int, int> ii;
 
-#define VIZ(u, i) (orig[inc[u][i]] != (u) ? \
-                   orig[inc[u][i]] : dest[inc[u][i]])
+#define MAXN 200010
 
-/* Input - FILL ME */
-/* aresta i = (u, v); orig[i] = u, dest[i] = v,
-   inc[u].push_back(i), inc[v].push_back(i) */
-vector<int> inc[MAXN];
-int orig[MAXM], dest[MAXM];
+// INPUT
+int n, m;
+vector<int> adj[MAXN];
 
-/* Output */
-int ponte[MAXM]; // ponte[i] -> aresta i é ponte
-int part[MAXN]; // part[u] -> vertice u é de articulação
-int ncomp; // numero de componentes biconexas
-int comp[MAXM]; // comp[i] = componente da aresta i
+// AUX
+bool vis[MAXN];
+int num[MAXN], low[MAXN];
+vector<int> pilha;
 
-/* Variaveis auxiliares */
-int low[MAXN], vis[MAXN], dt;
-stack<int> stck;
+// OUTPUT
+int ncomp;
+vector<int> comps[MAXN]
 
-/* Função auxiliar */
-int dfsbcc(int u, int p) {
-  int ch = 0;
-  vis[u] = dt++;
-  low[u] = vis[u];
-  for (int i = 0; i < (int) inc[u].size(); i++) {
-    int e = inc[u][i], v = VIZ(u, i);
-    if (!vis[v]) {
-      stck.push(e);
-      dfsbcc(v, u); ch++;
+void dfs(int u, int p, int& t) {
+  vis[u] = true;
+  num[u] = low[u] = t++;
+  pilha.push_back(u);
+  for (size_t i = 0; i < adj[u].size(); i++) {
+    int v = adj[u][i];
+    if (v == p)  continue;
+    if (vis[v]) {
+      low[u] = min(low[u], num[v]);
+    }
+    else {
+      dfs(v, u, t);
       low[u] = min(low[u], low[v]);
-      if (low[v] >= vis[u]) {
-        part[u] = 1;
-        ncomp++;
-        while (stck.top() != e) {
-          comp[stck.top()] = ncomp;
-          stck.pop();
+      if (low[v] > num[u]) IS_BRIDGE(u, v);
+      if (low[v] >= num[u] && (num[u] > 0 || num[v] > 1)) IS_ART(u);
+      if (low[v] >= num[u]) {
+        comps[ncomp].push_back(u);
+        while (pilha.back() != v) {
+          comps[ncomp].push_back(pilha.back());
+          pilha.pop_back();
         }
-        comp[stck.top()] = ncomp; stck.pop();
+        ncomp++;
       }
-      if (low[v] == vis[v]) ponte[e] = 1;
-    } else if (v != p) {
-      if (vis[v] < vis[u]) stck.push(e);
-      low[u] = min(low[u], vis[v]);
     }
   }
-  return ch;
 }
 
-void bcc(int n) {
-  memset(low, 0, sizeof(low));
-  memset(vis, 0, sizeof(vis));
-  memset(part, 0, sizeof(part));
-  memset(ponte, 0, sizeof(ponte));
-  memset(comp, 0, sizeof(comp));
-  dt = 1;
-  ncomp = 0;
-  for (int i = 0; i < n; i++)
-    if (!vis[i])
-      part[i] = dfsbcc(i, -1) >= 2;
-}
-
-#include <cstdio>
-
-int main(){
-  int i;
-  int n, m;
-  scanf("%d%d", &n, &m);
-
-  for (int i = 0; i < m; i++) inc[i].clear();
-  for (i = 0; i < m; i++) {
-    int u, v;
-    scanf("%d%d", &u, &v);
-    orig[i] = u; dest[i] = v;
-    inc[u].push_back(i);
-    inc[v].push_back(i);
-  }
-
-  bcc(n);
-
-  printf("Pontos de Articulacao:");
-  for (i = 0; i < n; i++)
-    if (part[i]) printf(" %d", i);
-  printf("\n");
-  
-  printf("Pontes:");
-  for (i = 0; i < m; i++)
-      if (ponte[i])
-        printf(" (%d %d)", orig[i], dest[i]);
-  printf("\n");
-
-  printf("Componentes:\n");
-  for (i = 0; i < m; i++)
-    printf("comp[%d] = %d\n", i, comp[i]);
-  printf("\n");
-
-  return 0;
+void bcc() {
+  int t;
+  int ncomp = 0;
+  for (int i = 0; i < n; i++) vis[i] = false;
+  for (int i = 0; i < n; i++) if (!vis[i]) dfs(i, -1, t = 0);
 }
