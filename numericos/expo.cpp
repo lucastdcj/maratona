44b47ca2eb68dec76a4d9db18ce915afff3ba07e
lucastdcj <lucastdcj@gmail.com>
2017-07-12 13:27:02 -0300

    Update expo.cpp

--- numericos/expo.cpp
+++ numericos/expo.cpp
@@ -10,10 +10,10 @@ Descricao: Dado a, b e m, calcula a^b mod m
 
 using namespace std;
 
-typedef long long int int64;
+typedef long long int ll;
 
-int64 expo(int64 a, int64 b, int64 m) {
-  int64 y = a % m, x = 1;
+ll expo(ll a, ll b, ll m = MOD) {
+  ll y = a % m, x = 1;
   while (b > 0) {
     if (b % 2 == 1) {
       x = (x*y) % m;
@@ -25,9 +25,8 @@ int64 expo(int64 a, int64 b, int64 m) {
 }
 
 /**** Exemplo simples de uso ****/
-int main()
-{
-  int64 res;
+int main() {
+  ll res;
   res = expo(2, 1000000007-2, 1000000007);
   printf("%lld\n", res);
   return 0;
